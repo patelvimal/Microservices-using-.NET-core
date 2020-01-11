c3b2b0ce3a2ccb95b460e5f38f06ac92982defdd
Vimal <vimal.patel.nvs@gmail.com>
2020-01-11 17:50:19 +0530

    updated project structure and added ServiceA and ServiceB

---
+++ src/Services/ServiceB/Controllers/ServiceAController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ServiceA.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ServiceBController : ControllerBase
+    {
+
+        public ServiceBController()
+        {
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "This is resultset from Service-B";
+        }
+    }
+}
